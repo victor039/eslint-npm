51519613dda5f49abae0e4bfec8341c96649ddc7
victor039 <tor-vic@hotmail.com>
2023-05-02 18:05:31 -0500

    uploading eslint files

---
+++ index.js
@@ -0,0 +1,16 @@
+const  nombre = "Victor"
+const  apellido = "Vergara"
+const  estudiante = nombre.concat(" ").concat(apellido) 
+
+const  estudianteMayus = estudiante.toUpperCase()
+const  estudianteMinus = estudiante.toLowerCase()
+
+const  cantLetras = estudiante.length 
+
+const  primeraLetra = nombre.at(0)
+
+const  ultimaLetraApellido = apellido.at(apellido.length -1)
+
+const  estudianteSinEspacios = estudiante.replace(" ", " ")
+const  nombreEnEstudiante = estudiante.includes(nombre)
+
